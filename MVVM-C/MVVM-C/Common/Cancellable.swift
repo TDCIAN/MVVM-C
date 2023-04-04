f68b43b42e4e7ca833fc7b2a1aa7ea056001ce36
TDCIAN <tdcian71@gmail.com>
2023-04-04 21:59:53 +0900

    make base folder structures

---
+++ MVVM-C/MVVM-C/Common/Cancellable.swift
@@ -0,0 +1,12 @@
+//
+//  Cancellable.swift
+//  MVVM-C
+//
+//  Created by JeongminKim on 2023/04/04.
+//
+
+import Foundation
+
+public protocol Cancellable {
+    func cancel()
+}
---
+++ MVVM-C/MVVM-C/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by JeongminKim on 2023/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
